2c7a5fcb8a630938e27a182907d1cdd0c63d458d
Dan Ho <igaryhe@gmail.com>
2020-06-17 03:14:16 +0800

    update tags

--- src/operator.rs
+++ src/operator.rs
@@ -3,61 +3,61 @@ use std::fmt;
 
 #[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq, Hash)]
 pub enum Tag {
-    #[serde(alias = "新手")]
+    #[serde(alias = "新手", rename(serialize = "新手"))]
     Starter,
-    #[serde(alias = "资深干员")]
+    #[serde(alias = "资深干员"), rename(serialize = "资深干员")]
     Senior,
-    #[serde(alias = "高级资深干员")]
+    #[serde(alias = "高级资深干员", rename(serialize = "高级资深干员"))]
     Top,
-    #[serde(alias = "近战位")]
+    #[serde(alias = "近战位", rename(serialize = "近战位"))]
     Melee,
-    #[serde(alias = "远程位")]
+    #[serde(alias = "远程位", rename(serialize = "远程位"))]
     Ranged,
-    #[serde(alias = "重装干员")]
+    #[serde(alias = "重装干员", rename(serialize = "重装干员"))]
     Defender,
-    #[serde(alias = "近卫干员")]
+    #[serde(alias = "近卫干员", rename(serialize = "近卫干员"))]
     Guard,
-    #[serde(alias = "先锋干员")]
+    #[serde(alias = "先锋干员", rename(serialize = "先锋干员"))]
     Vanguard,
-    #[serde(alias = "狙击干员")]
+    #[serde(alias = "狙击干员", rename(serialize = "狙击干员"))]
     Sniper,
-    #[serde(alias = "术师干员")]
+    #[serde(alias = "术师干员", rename(serialize = "术师干员"))]
     Caster,
-    #[serde(alias = "医疗干员")]
+    #[serde(alias = "医疗干员", rename(serialize = "医疗干员"))]
     Medic,
-    #[serde(alias = "辅助干员")]
+    #[serde(alias = "辅助干员", rename(serialize = "辅助干员"))]
     Supporter,
-    #[serde(alias = "特种干员")]
+    #[serde(alias = "特种干员", rename(serialize = "特种干员"))]
     Specialist,
-    #[serde(alias = "治疗")]
+    #[serde(alias = "治疗", rename(serialize = "治疗"))]
     Healing,
-    #[serde(alias = "支援")]
+    #[serde(alias = "支援", rename(serialize = "支援"))]
     Support,
-    #[serde(alias = "输出")]
+    #[serde(alias = "输出", rename(serialize = "输出"))]
     DPS,
-    #[serde(alias = "群攻")]
+    #[serde(alias = "群攻", rename(serialize = "群攻"))]
     AoE,
-    #[serde(alias = "减速")]
+    #[serde(alias = "减速", rename(serialize = "减速"))]
     Slow,
-    #[serde(alias = "生存")]
+    #[serde(alias = "生存", rename(serialize = "生存"))]
     Survival,
-    #[serde(alias = "防护")]
+    #[serde(alias = "防护", rename(serialize = "防护"))]
     Defense,
-    #[serde(alias = "削弱")]
+    #[serde(alias = "削弱", rename(serialize = "削弱"))]
     Debuff,
-    #[serde(alias = "位移")]
+    #[serde(alias = "位移", rename(serialize = "位移"))]
     Shift,
-    #[serde(alias = "控场")]
+    #[serde(alias = "控场", rename(serialize = "控场"))]
     CrowdControl,
-    #[serde(alias = "爆发")]
+    #[serde(alias = "爆发", rename(serialize = "爆发"))]
     Nuker,
-    #[serde(alias = "召唤")]
+    #[serde(alias = "召唤", rename(serialize = "召唤"))]
     Summon,
-    #[serde(alias = "快速复活")]
+    #[serde(alias = "快速复活", rename(serialize = "快速复活"))]
     FastRedeploy,
-    #[serde(alias = "费用回复")]
+    #[serde(alias = "费用回复", rename(serialize = "费用回复"))]
     DPRecovery,
-    #[serde(alias = "支援机械")]
+    #[serde(alias = "支援机械", rename(serialize = "志愿机械"))]
     Robot,
 }
 
